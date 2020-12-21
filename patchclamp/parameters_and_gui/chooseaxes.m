d11e8db71f9152002081b566d1b9e4677efbd603
Niraj Desai <niraj.s.desai@gmail.com>
2020-12-20 21:36:44 -0500

    Add files via upload

--- patchclamp/parameters_and_gui/chooseaxes.m
+++ patchclamp/parameters_and_gui/chooseaxes.m
@@ -14,19 +14,12 @@ if strcmp(DAQPARS.MainApp.popupSwitch_1.Value,'On') && ~isempty(find(ax1==1, 1))
     end
     if isempty(foo)
         app = graph1;
+        setScales(app,0);
     else
         app = foo.RunningAppInstance;
+        setScales(app,1);
     end
     ax(1) = app.UIAxes;
-    if DAQPARS.duration > 10000
-        app.timescaleKnob.Value = '100000';
-    elseif DAQPARS.duration > 1000
-        app.timescaleKnob.Value = '10000';
-    else
-        app.timescaleKnob.Value = '1000';
-    end
-    app.startingtimeSlider.Limits = [0, DAQPARS.duration];
-    pushValuesChangedButton(app);
     popup = true;
 end
 
@@ -40,18 +33,11 @@ if strcmp(DAQPARS.MainApp.popupSwitch_2.Value,'On') && ~isempty(find(ax1==2, 1))
     end
     if isempty(foo)
         app = graph2;
+        setScales(app,0);
     else
         app = foo.RunningAppInstance;
+        setScales(app,1);
     end
     ax(2) = app.UIAxes;
-    if DAQPARS.duration > 10000
-        app.timescaleKnob.Value = '100000';
-    elseif DAQPARS.duration > 1000
-        app.timescaleKnob.Value = '10000';
-    else
-        app.timescaleKnob.Value = '1000';
-    end
-    app.startingtimeSlider.Limits = [0, DAQPARS.duration];
-    pushValuesChangedButton(app);
     popup = true;
 end
