b6e3a74f508e7cf819c864567771341c1c95b61b
Niraj Desai <niraj.s.desai@gmail.com>
2021-03-10 15:19:14 -0500

    Add files via upload

--- patchclamp/calculateahp.m
+++ patchclamp/calculateahp.m
@@ -1,13 +1,27 @@
-function [ahp,ahpLatency] = calculateahp(data,locs,thrs,dt)
+function [ahp,ahpLatency,ahpHalfWidth,ahpThirdWidth] = calculateahp(data,locs,thrs,dt)
 
 locs = round(locs/dt);
+spikewidth = round(2/dt);
 
 if numel(locs)>1
-    [minV,idx] = min(data(locs(1):locs(2)));
+    data = data(locs(1)+spikewidth:locs(2)-spikewidth);
 else
-    [minV,idx] = min(data(locs(1):end));
+    data = data(locs(1)+spikewidth:end);
 end
+xdata = (1:length(data))*dt - dt;
+data = smooth(xdata,data,0.1,'loess');
+[minV,idx] = min(data);
 
 ahp = thrs(1)-minV;
 
-ahpLatency = idx*dt;
+ahpLatency = idx*dt + spikewidth;
+
+ahpHalf = thrs(1) - ahp/2;
+idx = find(data < ahpHalf);
+ahpHalfWidth = xdata(max(idx)) - xdata(min(idx));
+
+ahpThird = thrs(1) - ahp*2/3;
+idx = find(data < ahpThird);
+ahpThirdWidth = xdata(max(idx)) - xdata(min(idx));
+
+
