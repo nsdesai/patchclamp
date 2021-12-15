bbf6f4ed740438ecaa964b430b5e8b7f05509915
Niraj Desai <niraj.s.desai@gmail.com>
2021-12-15 12:35:46 -0500

    Add files via upload

--- patchclamp/daq_board/startdaq.m
+++ patchclamp/daq_board/startdaq.m
@@ -32,6 +32,7 @@ plotoutputs(recordingCounter)
 progressSoFar = recordingCounter / obj.TasksToExecute;
 if ~isempty(progressDialog)
     progressDialog.Value = progressSoFar;
+    progressDialog.Message = ['Completed ',num2str(recordingCounter),' of ',num2str(obj.TasksToExecute)];
 end
 recordingCounter = recordingCounter + 1;
 try
