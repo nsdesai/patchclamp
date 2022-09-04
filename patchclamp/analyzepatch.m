7384d35b6aaca9968809f45edde5e78d69e61494
Niraj Desai <niraj.s.desai@gmail.com>
2022-09-04 11:38:53 -0400

    Add files via upload

--- patchclamp/analyzepatch.m
+++ patchclamp/analyzepatch.m
@@ -1,4 +1,14 @@
 function [] = analyzepatch
+global analysisFolder %#ok<GVMIS> 
+
+% add all the folders
+analysisFolder = [userpath,filesep,'patchclamp',filesep,'analysis_files'];
+assert(isfolder(analysisFolder),...
+    'The folder containing the analysis files could not be found.')
+addpath(genpath(analysisFolder))
+filtersFolder = [userpath,filesep,'patchclamp',filesep,'filters'];
+addpath(genpath(filtersFolder))
+
 
 LCSMS_analysis
 
